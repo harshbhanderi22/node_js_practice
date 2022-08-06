264dee044f75782290df5ad7f1bf847126253a11
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-08-06 19:37:33 +0530

    HTML pages rendered successfully

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nodejs_practice
 It is simple repository where i will code of NodeJs which I am learning for a backend of flutter applications.<br>
-This is for understanding of how we can display html and json data and how can we link pages 
+This is for understanding of how we can display html pages for website<br>
+for home pages, any page named index.html will automatically become home page if we have not defined manually and it will be displayed when we use localhost without any route <br>
+ex. localhost:5000, This will automatically load index.html
--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 const express = require('express');
 //To make express executable
 const app = express();
+const path = require('path');
 
-//Using get to display information
-app.get('', (req, resp) => {
+const webpath = path.join(__dirname, 'webPage');
 
-    //To send request parameter
-    req.query.name = "harsh";
-    //Recieve Response, Displaying HTML data
-    resp.send(`
-        <h1>Welcome to homepage, ${req.query.name}</h1><br>
-        <a href="/aboutus">Go to about us page </a>
-    `)
-})
+app.use(express.static(webpath));
 
-//give route to about us which will give this data on about us page
-app.get('/aboutus', (req, resp) => {
-
-    //Displaying JSON data
-    resp.send(`
-    [
-        {
-            school : "PPSU",
-            course: "IT"
-        }
-        {
-            name : "Harsh",
-            email : "harsh@gmail.com"
-        }
-    ]
-    <a href="/">Go to Home page </a>
-    `)
-})
-
-//Listen server on localhost
 app.listen(5000);
