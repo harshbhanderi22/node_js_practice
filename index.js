6c95cc533f98b1162c0bc54a87f40d292bac8cf4
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-08-06 17:27:55 +0530

    crud operations perfomed on file

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-//__dirname will return current folder path where as files will be added to path
-const dirpath = path.join(__dirname, 'files');
+//Path to crud Function
+const dirPath = path.join(__dirname, 'crud');
 
-//Create Files
-for (i = 1; i <= 5; i++){
-    fs.writeFileSync(dirpath + '/file' + i + '.txt', "This is simple text file to understand the use of path module");
-}
+//Path to data.txt file
+const filepath = `${dirPath}/data.txt`;
 
-//ReadFiles List
-fs.readdir(dirpath, (error, files) => {
-    files.forEach((item) => {
-        console.log(item);  
-    })
-});
+//CreateFile
+fs.writeFileSync(filepath, "It is simple data file");
 
-//ReadFile List
-fs.readdir(dirpath, (error, datas) => {
-    console.log(datas);
-});
+//ReadFile
+fs.readFile(filepath, 'utf8', (erroe,item) => {
+    console.log(item)
+})
+
+//Update File content
+fs.appendFile(filepath, " and it has nothing else", (error) => {
+    if (!error) {
+        console.log("File Updated")        
+    }
+})
+
+//Rename File
+fs.rename(filepath, `${dirPath}/value.txt`, (error) => {
+    if (!error) {
+        console.log("File Name Updated")
+    }
+})
+
+//To delete file
+// fs.unlinkSync(`${dirPath}/value.txt`)
