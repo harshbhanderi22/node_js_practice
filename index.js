5f399a2cab5ba8cf27b3f5a76237a30e2727c799
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-08-12 20:37:34 +0530

    template engine EJS used

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const path = require('path');
 
 const webpath = path.join(__dirname, 'webPage');
 
+//Set the engine to tell Node that which engine we are using
+app.set('view engine', 'ejs');
+
+
 //Load given file at home route
 app.get('', (req, resp) => {
     resp.sendFile(`${webpath}/index.html`)
 })
+
+//It is compulsory to create folder named view to render this file using engine and file extension should be .ejs
+app.get('/profile', (req, resp) => {
+    const user = {
+        name: "Harsh Bhanderi",
+        branch: 'Engineering',
+        college: 'ppsu'
+   }
+    resp.render('profile.ejs', {user}) 
+});
 
 //Load about file and here in url we just need to add about not about.html
 app.get('/about', (req, resp) => {
