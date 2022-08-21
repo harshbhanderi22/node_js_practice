fe1317e9d7a2fe0aecf90704373e316121742704
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-08-21 11:01:04 +0530

    Basic Get API developed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # nodejs_practice
 Connect mongoDB in node js<br>
-In this module,We have learnt how to delete data in mongo from Node.
+In this branch, I have learnt about how can we use get method in API and connnect database with it which makes it dynamic API and can we use for realtime usecases.
---
+++ api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
+
+
+//Get API to get data from our databse and display it, we can pass query parameter but cannot pass body in get API.
+app.get('/',async (req, resp) => {
+    let db = dbConnect();
+    const result = await (await db).find({'name':'harsh'}).toArray();
+    resp.send(result);
+})
+
+app.listen(5000);
