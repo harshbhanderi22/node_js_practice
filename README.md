25f3b216b14e839e858c14f0f2334aaa1716b6b8
Harsh Bhanderi <harshbhanderi188@gmail.com>
2022-08-25 21:57:37 +0530

    All API are working fine

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # nodejs_practice
 Connect mongoDB in node js<br>
-In this branch, I have learnt about how we can develop post API using mongoose and also worked on code readility by seperating files.
+In this branch, I have learnt about how we can develop get, put, dele API using mongoose and also worked on code readility by seperating files.
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const login = require('./login_model');
 const app = express();
 
 app.use(express.json());
-app.post('/',async (req, resp) => {
 
+//Post API in mongoose
+app.post('/', async (req, resp) => {
     //We can pass direct data or can pass req.body if we are passing data from body (in postman)
     const data = new login(
         {
@@ -14,9 +15,31 @@ app.post('/',async (req, resp) => {
             role: 'broker'
         }
     )
-    const result =await data.save();
+    const result = await data.save();
     console.log(result);
     resp.send(result);
+});
+
+app.get('/get', async (req, resp) => {
+    const data = await login.find();
+    resp.send(data);
+})
+
+app.put('/put', async (req, resp) => {
+    const data = await login.updateOne(
+        {email: 'harsh@developer.com'},
+        {$set:  { email:'harsh@bhanderi.com' }}
+    )
+    resp.send(data);
+})
+
+//It is used to directly pass id in paramter
+//For Example: http://localhost:5000/delete/6301c049bc414739858a315e
+app.delete('/delete/:_id',async (req, resp) => {
+    const data = await login.deleteOne(
+        req.params
+    );
+    resp.send(data);
 })
 
 app.listen(5000);
